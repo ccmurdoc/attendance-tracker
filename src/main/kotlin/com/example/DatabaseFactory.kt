401b9196e75dbbda8f53b4060098446ba2bb6796
Chantal Murdock <chantalcmurdock@gmail.com>
2025-03-02 15:36:31 -0500

    Updated DatabaseFactory to use the provided PostgreSQL URL

--- src/main/kotlin/com/example/DatabaseFactory.kt
+++ src/main/kotlin/com/example/DatabaseFactory.kt
@@ -1,4 +1,4 @@
-package com.example
+/*package com.example
 
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
@@ -14,6 +14,69 @@ object DatabaseFactory {
     fun init() {
         val database = Database.connect("jdbc:sqlite:attendance.db", driver = "org.sqlite.JDBC")
         transaction(database) {
+            SchemaUtils.create(Students)
+        }
+    }
+}
+postgres://u364mlq88bmd:p79335d4abcf939f7a9959cd2a4ef9edfd5045c513675b2634aea1825af57ed64@cd5gks8n4kb20g.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d2v52ltivj1hsi
+
+object DatabaseFactory {
+    fun init() {
+        val databaseUrl = System.getenv("DATABASE_URL")
+
+        if (databaseUrl != null && databaseUrl.startsWith("postgres://")) {
+            //  Convert Heroku DATABASE_URL to proper JDBC format
+            val uri = URI(databaseUrl)
+            val userInfo = uri.userInfo.split(":")
+            val username = userInfo[0]
+            val password = userInfo[1]
+            val dbUrl = "jdbc:postgresql://${uri.host}:${uri.port}${uri.path}?sslmode=require"
+
+            Database.connect(dbUrl, driver = "org.postgresql.Driver", user = username, password = password)
+        } else {
+            // Use SQLite locally
+            Database.connect("jdbc:sqlite:attendance.db", driver = "org.sqlite.JDBC")
+        }
+
+        transaction {
+            SchemaUtils.create(Students)
+        }
+    }
+}
+*/
+package com.example
+
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
+import java.net.URI
+
+object Students : Table() {
+    val id = integer("id").autoIncrement()
+    val name = varchar("name", 255)
+    val attended = bool("attended")
+    override val primaryKey = PrimaryKey(id)
+}
+
+object DatabaseFactory {
+    fun init() {
+        val databaseUrl = System.getenv("DATABASE_URL") ?:
+        "postgres://u364mlq88bmd:p79335d4abcf939f7a9959cd2a4ef9edfd5045c513675b2634aea1825af57ed64@cd5gks8n4kb20g.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d2v52ltivj1hsi"
+
+        val db = if (databaseUrl.startsWith("postgres://")) {
+            //  Convert PostgreSQL URL to JDBC format
+            val uri = URI(databaseUrl)
+            val userInfo = uri.userInfo.split(":")
+            val username = userInfo[0]
+            val password = userInfo[1]
+            val dbUrl = "jdbc:postgresql://${uri.host}:${uri.port}${uri.path}?sslmode=require"
+
+            Database.connect(dbUrl, driver = "org.postgresql.Driver", user = username, password = password)
+        } else {
+            // Use SQLite for local development (optional)
+            Database.connect("jdbc:sqlite:attendance.db", driver = "org.sqlite.JDBC")
+        }
+
+        transaction(db) {
             SchemaUtils.create(Students)
         }
     }
